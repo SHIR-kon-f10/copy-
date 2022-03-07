4f560673197f3e7de444b24953f62432a26f2c74
Drakontiy <49278813+Drakontiy@users.noreply.github.com>
2022-03-08 01:33:47 +0300

    formfixnadoeloblin

--- edualpha.py
+++ edualpha.py
@@ -165,4 +165,4 @@ def load_recomendations():
 
 @app.route("/form/", methods=["POST","GET"])
 def Form():
-    return render_template("Form.html", FormNum= request.cookies.get("FOrm_id"))
+    return render_template("Form.html", FormNum= request.cookies.get("Form_id"))
